0d32cdb110b66c881d5c8731be3c0515c0c4ec4f
huai zhu <138862916+riceshowerX@users.noreply.github.com>
2024-05-16 14:45:21 +0800

    1.4.5

--- archive.php
+++ archive.php
@@ -1,48 +1,41 @@
 <?php
 
-/**
- * The template for displaying archive pages.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Akina
- */
+get_header();
 
-get_header(); ?>
+?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
-		<?php
-		if (have_posts()) : ?>
+		<?php if (have_posts()) : ?>
 
-			<?php if (!iro_opt('patternimg') || !z_taxonomy_image_url()) { ?>
+			<?php // 判断是否显示分类标题和描述 ?>
+			<?php if (!iro_opt('patternimg') || !z_taxonomy_image_url()) : ?>
 				<header class="page-header">
 					<h1 class="cat-title"><?php single_cat_title('', true); ?></h1>
 					<span class="cat-des">
 						<?php
-						if (category_description() != "") {
-							echo "" . category_description();
+						$category_description = category_description();
+						if (!empty($category_description)) {
+							echo $category_description;
 						}
 						?>
 					</span>
 				</header><!-- .page-header -->
-			<?php } // page-header 
-			?>
+			<?php endif; ?>
 
-			<?php
-			if (iro_opt('image_category') && is_category(explode(',', iro_opt('image_category')))) {
+			<?php // 加载文章列表模板 ?>
+			<?php 
+			$post_display_style = iro_opt('post_list_style');
+			$is_image_category = is_image_category(); // 使用独立函数判断是否为图片分类
+			if (iro_opt('image_category') && $is_image_category) { 
 				while (have_posts()) : the_post();
 					get_template_part('tpl/content', 'category');
 				endwhile;
 			} else {
-				if (iro_opt('post_list_style') == 'akinastyle') {
-					while (have_posts()) : the_post();
-						get_template_part('tpl/content', get_post_format());
-					endwhile;
-				} else {
-					get_template_part('tpl/content', 'thumb');
-				}
+				while (have_posts()) : the_post();
+					get_template_part('tpl/content', $post_display_style == 'akinastyle' ? get_post_format() : 'thumb');
+				endwhile;
 			}
 			?>
 			<div class="clearer"></div>
@@ -54,15 +47,26 @@ get_header(); ?>
 		endif; ?>
 
 	</main><!-- #main -->
-	<?php if (iro_opt('pagenav_style') == 'ajax') { ?>
-		<div id="pagination" <?php if (iro_opt('image_category') && is_category(explode(',', iro_opt('image_category')))) echo 'class="pagination-archive"'; ?>><?php next_posts_link(__(' Previous', 'sakurairo')); ?></div>
+
+	<?php // 加载分页导航 ?>
+	<?php $pagination_style = iro_opt('pagenav_style'); ?>
+	<?php if ($pagination_style == 'ajax') : ?>
+		<div id="pagination" <?php if (iro_opt('image_category') && $is_image_category) echo 'class="pagination-archive"'; ?>><?php next_posts_link(__(' Previous', 'sakurairo')); ?></div>
 		<div id="add_post"><span id="add_post_time" style="visibility: hidden;" title="<?php echo iro_opt('page_auto_load', ''); ?>"></span></div>
-	<?php } else { ?>
+	<?php else : ?>
 		<nav class="navigator">
 			<?php previous_posts_link('<i class="fa-solid fa-angle-left"></i>') ?><?php next_posts_link('<i class="fa-solid fa-angle-right"></i>') ?>
 		</nav>
-	<?php } ?>
+	<?php endif; ?>
 </div><!-- #primary -->
 
+<?php get_footer(); ?>
+
 <?php
-get_footer();
+
+// 独立函数判断是否为图片分类
+function is_image_category() {
+	return is_category(explode(',', iro_opt('image_category')));
+}
+
+?>
--- author.php
+++ author.php
@@ -3,87 +3,98 @@
 get_header();
 
 ?>
-<div class="author_info">
-	<div class="avatar" style="height: 75px;width: 75px;">
-	    <?php echo get_avatar($author) ?>
-	</div>
-	<div class="author-center">
-		<h3><?php the_author() ?></h3>
-		<div class="description"><?php echo get_the_author_meta('description') ? get_the_author_meta('description') : __("No personal profile set yet","sakurairo"); ?></div>
-	</div>
+
+<div class="author-info">
+  <div class="author-avatar">
+    <?php echo get_avatar( get_the_author_meta( 'ID' ), 75 ); ?>
+  </div>
+  <div class="author-details">
+    <h3><?php the_author(); ?></h3>
+    <p class="author-description"><?php echo get_the_author_meta('description') ? get_the_author_meta('description') : __("No personal profile set yet","sakurairo"); ?></p>
+  </div>
 </div>
-<style>
-	.author_info {
-		margin-top: 50px;
-		overflow: hidden;
-		padding: 40px 0;
-		position: relative;
-		font-weight: var(--global-font-weight);
-	}
 
-	.author_info .avatar {
-		float: left;
-		margin-right: 30px;
-		margin-left: 0px;
-	}
-
-	.author_info .avatar img {
-		border-radius: 100%;
-		border: 2px solid #fff;
-		background: #fff;
-		vertical-align: middle;
-	}
-
-	.author_info .author-center {
-		line-height: 27px;
-		padding-top: 10px;
-	}
-
-	.author_info .author-center h3 {
-		font-weight: 700;
-		font-size: 30px;
-		line-height: 1;
-		margin-bottom: 5px;
-		display: inline;
-	}
-
-	.author_info .author-center .description {
-		font-size: 14px;
-		line-height: 2;
-		font-weight: var(--global-font-weight);
-	}
-</style>
 <div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
+  <main id="main" class="site-main" role="main">
 
-		<?php
-		if (have_posts()) :
-			/* Start the Loop */
-			if (iro_opt('post_list_style') == 'akinastyle') {
-				while (have_posts()) : the_post();
-					get_template_part('tpl/content', get_post_format());
-				endwhile;
-			} else {
-				get_template_part('tpl/content', 'thumb');
-			}
-		?>
-			<div class="clearer"></div>
-		<?php else :
+    <?php
+    if (have_posts()) :
+      // Start the Loop
+      if (iro_opt('post_list_style') == 'akinastyle') {
+        // 使用 akinastyle 模板
+        while (have_posts()) : the_post();
+          get_template_part('tpl/content', get_post_format());
+        endwhile;
+      } else {
+        // 使用缩略图模板
+        get_template_part('tpl/content', 'thumb');
+      }
+    ?>
+    <?php else :
 
-			get_template_part('tpl/content', 'none');
+      get_template_part('tpl/content', 'none');
 
-		endif; ?>
+    endif; ?>
 
-	</main><!-- #main -->
-	<?php if (iro_opt('pagenav_style') == 'ajax') { ?>
-		<div id="pagination"><?php next_posts_link(__(' Previous', 'sakurairo')); ?></div>
-		<div id="add_post"><span id="add_post_time" style="visibility: hidden;" title="<?php echo iro_opt('page_auto_load', ''); ?>"></span></div>
-	<?php } else { ?>
-		<nav class="navigator">
-			<?php previous_posts_link('<i class="fa-solid fa-angle-left"></i>') ?><?php next_posts_link('<i class="fa-solid fa-angle-right"></i>') ?>
-		</nav>
-	<?php } ?>
+  </main><!-- #main -->
+
+  <?php 
+  $pagenav_style = iro_opt('pagenav_style'); 
+  if ($pagenav_style == 'ajax') { 
+    // 使用 Ajax 分页
+    ?>
+    <div id="pagination">
+      <?php next_posts_link(__(' Previous', 'sakurairo')); ?>
+    </div>
+    <div id="add_post">
+      <span id="add_post_time" title="<?php echo iro_opt('page_auto_load', ''); ?>"></span>
+    </div>
+  <?php 
+  } else { 
+    // 使用传统分页
+    ?>
+    <nav class="navigator">
+      <?php previous_posts_link('<i class="fa-solid fa-angle-left"></i>'); ?>
+      <?php next_posts_link('<i class="fa-solid fa-angle-right"></i>'); ?>
+    </nav>
+  <?php } ?>
 </div><!-- #primary -->
 
 <?php
 get_footer();
+?>
+
+<style>
+  .author-info {
+    display: flex;
+    align-items: center;
+    margin-top: 50px;
+    padding: 40px 0;
+    font-weight: var(--global-font-weight);
+  }
+
+  .author-avatar {
+    margin-right: 30px;
+  }
+
+  .author-avatar img {
+    border-radius: 50%; /* 更圆润的头像 */
+    border: 2px solid #fff;
+    background: #fff;
+  }
+
+  .author-details {
+    line-height: 1.5;
+  }
+
+  .author-details h3 {
+    font-weight: 700;
+    font-size: 24px;
+    margin-bottom: 10px;
+    color: var(--primary-color); /* 使用主题颜色 */
+  }
+
+  .author-description {
+    font-size: 16px;
+  }
+</style>
