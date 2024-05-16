3258e76b03b27f8ea0595e50dff2c1e78f884aa8
huai zhu <138862916+riceshowerX@users.noreply.github.com>
2024-05-16 13:55:37 +0800

    1.4.5

--- page-links.php
+++ page-links.php
@@ -7,11 +7,27 @@
 get_header(); 
 
 ?>
+
+<?php 
+
+function display_link_items() {
+  echo get_link_items();
+}
+
+?>
+
 	<?php while(have_posts()) : the_post(); ?>
 	<?php $post = get_post(); ?>
-	<?php if(!iro_opt('patternimg') || !get_post_thumbnail_id(get_the_ID())) { ?>
+	<?php 
+	$hasPatternImg = iro_opt('patternimg');
+	$hasFeaturedImage = get_post_thumbnail_id(get_the_ID());
+
+	if(!$hasPatternImg || !empty($hasFeaturedImage)) { 
+		?>
 		<span class="linkss-title"><?php the_title();?></span>
-	<?php } ?>
+		<?php 
+	} 
+	?>
 		<article <?php post_class("post-item"); ?>>
 			<?php if (iro_opt('article_auto_toc', 'true') && check_title_tags($post->post_content)) { //加载目录?>
 			<div class="has-toc have-toc"></div>
@@ -20,10 +36,12 @@ get_header();
 				<?php the_content( '', true ); ?>
 			</div>			
 			<div class="links">
-				<?php echo get_link_items(); ?>
+				<?php display_link_items(); ?>
 			</div>
 		</article>
 	<?php get_template_part('layouts/sidebox'); //加载目录容器?> 
 	<?php endwhile; ?>
 <?php
 get_footer();
+
+?>
